4a98b16642516e10525214ad47a1e9df3d8b5cdc
Glamnart <favournchess192@gmail.com>
2026-03-10 00:57:56 +0100

    Added the function that finds the best student

--- assignment-1-es6-refactor/utilities.js
+++ assignment-1-es6-refactor/utilities.js
@@ -2,3 +2,13 @@ export function average(arr) { // Calculates Average of scores
     return arr.reduce((avg, {score}) => avg += (score/arr.length), 0);   
 }
 
+export function bestStudent(arr) // Finds best student
+{
+    let student;
+    const bestScore = arr.reduce((bestScore,{name,score}) => {
+        bestScore = Math.max(bestScore,score) // For the bigger score
+        bestScore == score ? student = name : null; // Saves the name of the bigger score temporarily till a bigger one comes along
+        return bestScore;
+    },0)
+    return student;
+}
