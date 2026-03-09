84d42d10d19beebdf476b6e48efdf65e88c46f62
Glamnart <favournchess192@gmail.com>
2026-03-09 22:16:14 +0100

    Reading the json files into index.js

--- assignment-2-async-dashboard/index.js
+++ assignment-2-async-dashboard/index.js
@@ -1,2 +1,17 @@
+import { readFile } from "node:fs/promises";
 
+async function dashboard() {
+    try{
+        const [orders,users] = await Promise.all([
+            readFile("data/orders.json", "utf-8"),
+            readFile("data/users.json", "utf-8")
+        ])
+    }
+    catch (error)
+    {
+        console.log(error)
+    }  
+}
+
+dashboard();
 
