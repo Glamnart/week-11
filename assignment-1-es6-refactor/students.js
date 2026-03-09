88c2bf2ab9ddc32e1fc9090105099c382674639c
Glamnart <favournchess192@gmail.com>
2026-03-10 00:51:19 +0100

    Created the file that holds the array of objects that contains students' information

---
+++ assignment-1-es6-refactor/students.js
@@ -0,0 +1,8 @@
+// An array of objects holding student's information
+export const students = [
+    {name: "Bethany", score: 76},
+    {name: "Philip", score: 20},
+    {name: "Badewa", score: 67},
+    {name: "James", score: 100},
+    {name: "Mary", score: 44}
+]
