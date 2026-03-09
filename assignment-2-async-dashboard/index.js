cc54917991f15e7ce75b7237196d77d28ea517ea
Glamnart <favournchess192@gmail.com>
2026-03-09 22:58:04 +0100

    Turned the read files back into arrays

--- assignment-2-async-dashboard/index.js
+++ assignment-2-async-dashboard/index.js
@@ -1,11 +1,17 @@
-import { readFile } from "node:fs/promises";
+import { readFile } from "node:fs/promises"; // Imports the readFile() function
 
 async function dashboard() {
     try{
-        const [orders,users] = await Promise.all([
+        // Reads both files *in parallel*
+        const [ordersRaw,usersRaw] = await Promise.all([
             readFile("data/orders.json", "utf-8"),
             readFile("data/users.json", "utf-8")
         ])
+
+        // Turns them back into arrays 
+        const orders = JSON.parse(ordersRaw);
+        const users = JSON.parse(usersRaw);
+
     }
     catch (error)
     {
