38b0dd86e17f0818a9c96b78459f07f999f0c6a6
Glamnart <favournchess192@gmail.com>
2026-03-10 00:45:31 +0100

    Adds the feature that searches for the best student to display

--- assignment-1-es6-refactor/legacy.js
+++ assignment-1-es6-refactor/legacy.js
@@ -19,5 +19,28 @@ for (let student of students){
 
 console.log(" ");
 
+//Gives best student 
+console.log("Best Student:");
+
+let bestStudent, bestScore;
+for (let i in students){
+   if (i == 0){
+    bestScore = students[i].score 
+    bestStudent = students[i].name
+   }
+   else 
+    {
+        if (students[i].score > bestScore)
+        {
+            bestScore = students[i].score 
+            bestStudent = students[i].name
+        }
+        else
+            continue;
+    } 
+}
+console.log("> " + bestStudent);
+
+console.log(" ");
 
 
