62b9c4c2308da040f7fdff88fdb6b6f2d4df8b26
Glamnart <favournchess192@gmail.com>
2026-03-09 23:43:27 +0100

    Adds the function that counts the number of orders in each type of status

--- assignment-2-async-dashboard/utilities.js
+++ assignment-2-async-dashboard/utilities.js
@@ -7,3 +7,14 @@ export function active(arr){ // Counts the number of active members
         const active = arr.filter(({isActive}) => isActive === true);
         return active.length
 } 
+
+export function count(arr){
+    const count = arr.reduce((count, {status})=>{
+        count[status] = (count[status] || 0) + 1
+        return count;
+    },{})
+     
+    const tableFormat = Object.entries(count).map(([status, count]) => ({ status, count }));
+
+    return tableFormat;
+}
