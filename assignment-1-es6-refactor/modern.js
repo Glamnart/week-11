eaa38e907b35b9046580f18120f7f0ae07f5d446
Glamnart <favournchess192@gmail.com>
2026-03-10 00:53:49 +0100

    Prints the names of people who passed

--- assignment-1-es6-refactor/modern.js
+++ assignment-1-es6-refactor/modern.js
@@ -1,3 +1,9 @@
 //  Import the array of objects holding student's information
 import { students } from "./students.js";
 
+//Gives students who passed
+console.log("Passed:");
+let passMark = 70;
+students.filter(({score}) => score >= passMark)
+        .map(({name}) => console.log(`> ${name}`));
+
