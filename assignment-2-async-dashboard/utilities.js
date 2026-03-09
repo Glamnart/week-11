4edcb09c67005d047014da93c7c0b5185283dc59
Glamnart <favournchess192@gmail.com>
2026-03-09 23:19:05 +0100

    Adds active members counter

--- assignment-2-async-dashboard/utilities.js
+++ assignment-2-async-dashboard/utilities.js
@@ -2,3 +2,12 @@ export function average(arr){ // Calculates the average
     const avg = arr.reduce((avg, {amount})=> avg + (amount/arr.length), 0)
     return avg.toFixed(2);
 }
+
+export function active(arr){
+        const active = arr.reduce((active, {isActive})=>{
+            active[isActive] = (active[isActive] || 0) + 1;
+            return active;
+        },{})
+        
+        return active.true 
+} 
