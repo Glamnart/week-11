7822ec54a9ca7703ced8bd7905dcbcf70181f695
Glamnart <favournchess192@gmail.com>
2026-03-10 00:43:52 +0100

    Create the array of objects that stores the students' names and scores

--- assignment-1-es6-refactor/legacy.js
+++ assignment-1-es6-refactor/legacy.js
@@ -1,3 +1,11 @@
+// An array of objects holding student's information
+const students = [
+    {name: "Bethany", score: 76},
+    {name: "James", score: 100},
+    {name: "Badewa", score: 67},
+    {name: "Mary", score: 44},
+    {name: "Philip", score: 20}
+]
 
 
 
