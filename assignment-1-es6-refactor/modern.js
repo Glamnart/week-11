383fa4ad414e119dd7f60e4a5fa11dcd559d1d31
Glamnart <favournchess192@gmail.com>
2026-03-10 01:09:57 +0100

    Used the write line break symbols

--- assignment-1-es6-refactor/modern.js
+++ assignment-1-es6-refactor/modern.js
@@ -7,26 +7,22 @@ let passMark = 70;
 students.filter(({score}) => score >= passMark)
         .map(({name}) => console.log(`> ${name}`));
 
-console.log(" ");
-
 import { bestStudent, average } from "./utilities.js";
 
 //Gives best student
-console.log("Best Student:");
+console.log("\nBest Student:");
 console.log(`> ${bestStudent(students)}`)
 
-console.log(" ");
 
 //Gives class average
-console.log("Class Average =");
+console.log("\nClass Average =");
 console.log(average(students).toFixed(2));
 
-console.log(" ");
 
 import { leaderBoard } from "./leaderBoard.js";
 
 //Class leader board 
 
 
-console.log("Class Leaderboard:")
+console.log("\nClass Leaderboard:")
 leaderBoard(students);
