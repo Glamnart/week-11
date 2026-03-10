76bbad2e40e08b166746877b0369fde31aa66ad2
Glamnart <favournchess192@gmail.com>
2026-03-10 01:06:45 +0100

    Adds spaces for styling

--- assignment-1-es6-refactor/modern.js
+++ assignment-1-es6-refactor/modern.js
@@ -7,7 +7,7 @@ let passMark = 70;
 students.filter(({score}) => score >= passMark)
         .map(({name}) => console.log(`> ${name}`));
 
-
+console.log(" ");
 
 import { bestStudent, average } from "./utilities.js";
 
@@ -15,13 +15,13 @@ import { bestStudent, average } from "./utilities.js";
 console.log("Best Student:");
 console.log(`> ${bestStudent(students)}`)
 
-
+console.log(" ");
 
 //Gives class average
 console.log("Class Average =");
 console.log(average(students).toFixed(2));
 
-
+console.log(" ");
 
 import { leaderBoard } from "./leaderBoard.js";
 
