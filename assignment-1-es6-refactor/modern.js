72d25ed7c9a7631828fb487083f07447a002a693
Glamnart <favournchess192@gmail.com>
2026-03-10 01:02:29 +0100

    Prints out the the average and best student

--- assignment-1-es6-refactor/modern.js
+++ assignment-1-es6-refactor/modern.js
@@ -1,5 +1,5 @@
 //  Import the array of objects holding student's information
-import { students } from "./students.js";
+import { students } from "./students_store.js";
 
 //Gives students who passed
 console.log("Passed:");
@@ -8,5 +8,14 @@ students.filter(({score}) => score >= passMark)
         .map(({name}) => console.log(`> ${name}`));
 
 
-
 import { bestStudent, average } from "./utilities.js";
+
+//Gives best student
+console.log("Best Student:");
+console.log(`> ${bestStudent(students)}`)
+
+
+
+//Gives class average
+console.log("Class Average =");
+console.log(average(students).toFixed(2));
